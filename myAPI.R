a058d120cc781e79801dcf324cef21923ebd3a32
Kyra Kapsaskis <kyrakapsaskis@Kyras-Air-3.lan>
2024-07-28 17:39:18 -0400

    working on modeling

--- myAPI.R
+++ myAPI.R
@@ -25,3 +25,17 @@ function(msg=""){
 }
 
 #http://localhost:PORT/echo?msg=Hey
+
+
+# predictor endpoint
+#* @param pred Choose a predictor
+#* @get /pred
+function(pred=""){
+  list(pred = paste0("The predictor is: '", pred, "'"))
+  
+}
+
+
+# docker image ls
+# docker run -p 8000:8000 rapi
+# http://127.0.0.1:8000/
